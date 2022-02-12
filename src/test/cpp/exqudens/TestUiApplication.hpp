52d1f4f626ca28613118ba0a5d1e2577b54d49ba
exqudens <exqudens@gmail.com>
2022-02-11 18:30:15 -0800

    step 20

---
+++ src/test/cpp/exqudens/TestUiApplication.hpp
@@ -0,0 +1,84 @@
+#pragma once
+
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <stdexcept>
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include "exqudens/TestMacros.hpp"
+
+namespace exqudens::vulkan {
+
+  class TestUiApplication {
+
+    private:
+
+      std::vector<std::string> arguments = {};
+      GLFWwindow* window = nullptr;
+
+    public:
+
+      TestUiApplication(const int& argc, char** argv) {
+        try {
+          for (std::size_t i = 0; i < argc; i++) {
+            arguments.emplace_back(std::string(argv[i]));
+          }
+        } catch (...) {
+          std::throw_with_nested(std::runtime_error(CALL_INFO()));
+        }
+      }
+
+      int run() {
+        try {
+          int width = 800;
+          int height = 600;
+          std::string title = "Vulkan";
+
+          glfwInit();
+          glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+          window = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
+
+          glfwSetWindowUserPointer(window, this);
+          glfwSetFramebufferSizeCallback(window, frameBufferResizeCallback);
+
+          while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+            int width = 0;
+            int height = 0;
+            glfwGetFramebufferSize(window, &width, &height);
+            while (width == 0 || height == 0) {
+              glfwGetFramebufferSize(window, &width, &height);
+              glfwWaitEvents();
+            }
+            //vulkanEnvironment->setFrameBufferSize(width, height);
+            //vulkanEnvironment->drawFrame();
+          }
+          //vulkanEnvironment->waitIdle();
+
+          //delete vulkanEnvironment;
+          glfwDestroyWindow(window);
+          glfwTerminate();
+
+          return EXIT_SUCCESS;
+        } catch (...) {
+          std::throw_with_nested(std::runtime_error(CALL_INFO()));
+        }
+      }
+
+    private:
+
+      static void frameBufferResizeCallback(GLFWwindow* window, int width, int height) {
+        try {
+          //TestUiApplication* app = reinterpret_cast<TestUiApplication*>(glfwGetWindowUserPointer(window));
+        } catch (...) {
+          std::throw_with_nested(std::runtime_error(CALL_INFO()));
+        }
+      }
+
+  };
+
+}
--- src/test/cpp/exqudens/test/UiTests.hpp
+++ src/test/cpp/exqudens/test/UiTests.hpp
@@ -1,11 +1,19 @@
 #pragma once
 
+#include <cstdlib>
+
 #include <gtest/gtest.h>
+
+#include "exqudens/TestConfiguration.hpp"
+#include "exqudens/TestUiApplication.hpp"
 
 namespace exqudens::vulkan {
 
   TEST(UiTests, test1) {
-    ASSERT_EQ(1, 2);
+    int argc = 0;
+    char** argv = nullptr;
+    int result = TestUiApplication(argc, argv).run();
+    ASSERT_EQ(EXIT_SUCCESS, result);
   }
 
   TEST(UiTests, test2) {
